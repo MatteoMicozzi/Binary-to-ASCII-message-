4f93ba834005f6caff96170e9dccf47d410d1e9f
MatteoMicozzi <micozzimatteo@yahoo.it>
2021-11-16 02:16:00 +0000

    Refactoring for binary to decimal.

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,15 @@
 def binary_to_decimal(binary)
   values = [1, 2, 4, 8, 16, 32, 64, 128]
   bit_position = 0
-  value = 0
-  code = binary.split(//).reverse!
+  decimal_value = 0
 
+  code = binary.split(//).reverse!
   code.each { |bit|
     if bit == '1'
-      value += values[bit_position]
+      decimal_value += values[bit_position]
     end
     bit_position += 1
   }
 
-  return value
+  return decimal_value
 end
