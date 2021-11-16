a0dee36582e990d5ae7c2507a1ec48929166b959
MatteoMicozzi <micozzimatteo@yahoo.it>
2021-11-16 01:25:52 +0000

    Passing 1st test for binary to decimal.

---
+++ lib/project.rb
@@ -0,0 +1,3 @@
+def binary_to_decimal(binary)
+    return binary.to_i
+end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,8 +1,8 @@
 require 'require_all'
 require_all 'lib'
 
-describe '' do
-  it '' do
-    expect().to eq()
+describe 'binary_to_decimal' do
+  it 'would return from the input "0" the output 0' do
+    expect(binary_to_decimal("0")).to eq(0)
   end
 end
